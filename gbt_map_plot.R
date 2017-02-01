ef7bb113b31eb58a7deafdad0b77bf2850b73983
kbseah <kbseah@users.noreply.github.com>
2017-02-01 13:19:10 +0100

    Changes to plot output  * Omit legend  * Change plot char to open circles  * Add text legend with number of mapped reads

--- gbt_map_plot.R
+++ gbt_map_plot.R
@@ -16,30 +16,43 @@ outfile <- args[5]
 lib <- args[6]
 dom <- args[7]
 
-
 ## Load library
 library(gbtools)
 
 ## Load data
 d <- gbt(covstats, ssu=ssutab)
 mapsRaw <- read.table(mapstats, sep="\t", header=F)
-maps <- data.frame(mapsRaw$V1,mapsRaw$V6)
+maps <- data.frame(ID=mapsRaw$V1,Maps=mapsRaw$V6)
 
 ## Prepare palette and legend
 blackred = colorRampPalette(c("Black","Red"))
-legendArr = c(0.25,0.5,0.75,1)
-legendArr = c(1,round(legendArr * max(maps[,2])))
-legendCol = sapply(legendArr,function(x) blackred(max(maps[,2]))[x])
+#legendArr = c(0.25,0.5,0.75,1)
+#legendArr = c(1,round(legendArr * max(maps[,2])))
+#legendCol = sapply(legendArr,function(x) blackred(max(maps[,2]))[x])
 
 ## Do plot
 png(file=outfile)
-plot(d, ssu=T, marker=F, textlabel=F, assemblyName=paste(lib,dom,sep=" "))
+plot(d, ssu=F, marker=F, textlabel=F, assemblyName=paste(lib,dom,sep=" "))
 for (i in dim(maps)[1]:1) { # Reverse sort
-    points(gbtbin(as.character(maps[i,1]),
-                  d,
-                  slice=1),
-           cutoff=200,
-           col=blackred(max(maps[,2]))[maps[i,2]])
+    # Extract data for contigs with mapped reads from gbt object
+    tmpbin <- gbtbin(as.character(maps[,1]),
+                     d,
+                     slice=1)
+    tmpbin2 <- merge(tmpbin$scaff,maps,by.x="ID",by.y="ID")
+    # Render points for contigs with mapped reads of interest
+    points(x=tmpbin2$Ref_GC,
+           y=tmpbin2$Avg_fold,
+           pch=21,
+           cex=sqrt(tmpbin2$Length)/100,
+           col=blackred(max(tmpbin2$Maps))[tmpbin2$Maps])
+    # Display text label only if more than 10 reads mapping
+    tmpbin3 <- subset(tmpbin2,Maps > 10)
+    # Render text label
+    text(x=tmpbin3$Ref_GC,
+         y=tmpbin3$Avg_fold,
+         as.character(tmpbin3$Maps),
+         cex=0.75,
+         pos=4)
 }
-legend("topleft",legend=legendArr,fill=legendCol,cex=0.75,title="Reads mapped")
+#legend("topleft",legend=legendArr,fill=legendCol,cex=0.75,title="Reads mapped")
 dev.off()
